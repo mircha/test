1c1d8c222fee7d1c0a11c8b60ac013163646124a
Mircea Bordean <mirceab87@gmail.com>
2022-06-13 22:02:43 +0300

    Add files via upload

--- api/test.js
+++ api/test.js
@@ -1,14 +1,4 @@
-const express = require('express');
-const router = express.Router();
-
-const port = process.env.port || 3000
-
-router.get('/', (req, res) => {
-    res.send('Hello World');
-})
-
-router.get('/test', (req, res) => {
-  res.send('Port: ' + port);
-})
-
-module.exports = router;
+export default function test(req, res) {
+  res.statusCode = 200;
+  res.json({ message: 'It works' });
+}
