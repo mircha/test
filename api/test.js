99141e042aa4825ce755d17130f6018c7c070bf8
Mircea Bordean <mirceab87@gmail.com>
2022-06-13 22:09:37 +0300

    Add files via upload

--- api/test.js
+++ api/test.js
@@ -1,10 +1,7 @@
 export default function test(req, res) {
-  res.statusCode = 200;
-  //res.json({ message: 'It works' });
-
   const kanye = await require('../stuff/kanye').getKanieRest();
   const quote = await require('../stuff/quote').fetchQuote();
-
+  res.statusCode = 200;
   res.json({ kanye: kanye, quote: quote });
 }
 
