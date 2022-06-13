a3b54a9d15c780f2e32f6a8120f0655a6ffa64cd
Mircea Bordean <mirceab87@gmail.com>
2022-06-13 22:03:14 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const test = require('./api/test')
-const port = process.env.port || 3000
-
-app.use("/api/test", test)
-
-app.listen(port, ()=>{console.log(`Listening on port ${port}`)})
