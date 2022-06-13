10d813675d9454f483fe4afdf0465d3a66135c98
Mircea Bordean <mirceab87@gmail.com>
2022-06-13 22:21:32 +0300

    Delete stuff directory

--- stuff/kanye.js
+++
@@ -1,8 +0,0 @@
-const fetch = require('node-fetch')
-const getKanieRest = async () => {
-    const response = await fetch('https://api.kanye.rest')
-    const data = await response.json()
-    return data
-}
-
-module.exports.getKanieRest = getKanieRest
--- stuff/quote.js
+++
@@ -1,8 +0,0 @@
-const fetch = require('node-fetch')
-const fetchQuote = async () => {
-    const response = await fetch('https://api.quotable.io/random')
-    const data = await response.json()
-    return data
-}
-
-module.exports.fetchQuote = fetchQuote
